0bfeef88ac0756d719f5b6e2b9b354c22418e1ed
zhangj-bmg <40463332+zhangj-bmg@users.noreply.github.com>
2018-06-22 15:41:55 +1000

    update
    dfd

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,8 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Test
+        
+        
+        ' Not like this
     End Sub
 End Class
